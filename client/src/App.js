f50064696bcedb385ab644421ab829692ad820d7
Christos Deretzis <deretzischristos@gmail.com>
2021-10-16 00:45:13 +0300

    make home route only for authenticated users:

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Login from './Components/Login/Login';
 import SignUp from './Components/SignUp/SignUp';
@@ -13,16 +13,28 @@ import {
 } from "react-router-dom";
 
 function App() {
+  const [isAuth, setAuth] = useState(false);
 
-  const cookie = new Cookies();
-  const jwt = cookie.get('jwt');
-  console.log(jwt);
+  const checkAuth = () => {
+     // Your auth logic here
+     const cookies = new Cookies();
+     const token = cookies.get('jwt');
+     if(token){
+        setAuth(true);
+     }
+  };
+
+  useEffect(() => {
+    checkAuth();
+  });
 
   return (
     <Router>
       <Switch>
-        <Redirect exact from="/" to={"/home"} />
-        <Route exact path="/home" component={Home} />
+        <Redirect exact from="/" to="/login"/>
+        <Route exact path="/home" render={(props) =>
+            isAuth ? <Home {...props} /> : <Redirect to="/" />
+          }/>
         <Route exact path="/login" component={Login} />
         <Route exact path="/signup" component={SignUp} />
       </Switch>
